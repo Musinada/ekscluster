c45c39c8c79a5b4ef72e98b995822fc8ff7bb028
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-06-02 00:48:52 +0530

    Update VPC_with_EC2.tf

--- VPC_with_EC2.tf
+++ VPC_with_EC2.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "demo-server" {
  key_name = var.key 
  instance_type  = var.instance-type
  associate_public_ip_address = true
-subnet_id = aws_subnet.demo_subnet.id
-vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
+ subnet_id = aws_subnet.demo_subnet.id
+ vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
 }
 
 // Create VPC
